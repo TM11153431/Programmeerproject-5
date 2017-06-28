cc564787cc8f075e393b945781e18cf0c2608213
smeber <sanne@berendschot.nl>
2017-06-29 00:07:58 +0200

    Add files via upload

--- Code/refugees266.js
+++ Code/refugees266.js
@@ -2131,18 +2131,14 @@ function setTitleYAxisGraphTotal(title) {
         .text(title);
 };
 
-
-/////
-
-
-
-
-
+// add the toooltip for the total timeline
 function addTooltipTimelineTotal() {
-   
+    
+    // make svg for tooltip
     svgTooltipTimelineTotal = svgGraphTotal.append("g")
         .style("display", "none");
 
+    // append vertical line
     svgTooltipTimelineTotal.append("line")
         .attr("class", "xTooltip")
         .style("stroke", "black")
@@ -2151,6 +2147,7 @@ function addTooltipTimelineTotal() {
         .attr("y1", 0)
         .attr("y2", heightG);
 
+    // append horizontal line
     svgTooltipTimelineTotal.append("line")
         .attr("class", "yTooltip")
         .style("stroke", "black")
@@ -2159,36 +2156,26 @@ function addTooltipTimelineTotal() {
         .attr("x1", widthG)
         .attr("x2", widthG);
 
+    // append circle
     svgTooltipTimelineTotal.append("circle")
         .attr("class", "yTooltip")
         .style("fill", "none")
         .style("stroke", "black")
         .attr("r", 4);
 
-    svgTooltipTimelineTotal.append("text")
-        .attr("class", "y1Timeline")
-        .style("stroke", "white")
-        .style("stroke-width", "3.5px")
-        .style("opacity", 0.8)
-        .attr("dx", 8)
-        .attr("dy", "-.3em");
+    // add text for amount
     svgTooltipTimelineTotal.append("text")
         .attr("class", "textAmount")
         .attr("dx", 8)
         .attr("dy", "-.3em");
 
-    svgTooltipTimelineTotal.append("text")
-        .attr("class", "y3")
-        .style("stroke", "white")
-        .style("stroke-width", "3.5px")
-        .style("opacity", 0.8)
-        .attr("dx", 8)
-        .attr("dy", "1em");
+    // add text for year
     svgTooltipTimelineTotal.append("text")
         .attr("class", "textYear")
         .attr("dx", 8)
         .attr("dy", "1em");
 
+    // append tooltip to graph
     svgGraphTotal.append("rect")
         .attr("width", widthG)
         .attr("height", heightG)
@@ -2197,44 +2184,52 @@ function addTooltipTimelineTotal() {
         .on("mouseover", function() { svgTooltipTimelineTotal.style("display", null); })
         .on("mousemove", mousemoveTotal)
         .on("mouseout", function() { svgTooltipTimelineTotal.style("display", "none"); });
-    
-    function mousemoveTotal() {
+};
 
-        yG.domain([0, maxDataGraphTotalAmount]); 
+// change tooltip when mouse is moved
+function mousemoveTotal() {
 
-        var x0 = xG.invert(d3.mouse(this)[0]),
-        i = bisectDate(dataTotal, x0, 1),
-        d0 = dataTotal[i - 1],
-        d1 = dataTotal[i],
-        d = x0 - d0.year > d1.year - x0 ? d1 : d0;  
+    // set correct domain
+    yG.domain([0, maxDataGraphTotalAmount]); 
 
-        var amount = +d.amount
+    // select correct year and data
+    var timeMouse = xG.invert(d3.mouse(this)[0]),
+    i = bisectDate(dataTotal, timeMouse, 1),
+    dataLeft = dataTotal[i - 1],
+    dataRight = dataTotal[i],
+    d = timeMouse - dataLeft.year > dataRight.year - timeMouse ? dataRight : dataLeft;  
 
-        svgTooltipTimelineTotal.select("circle.yTooltip")
-            .attr("transform", "translate(" + xG(d.year) + "," + yG(d.amount) + ")");
+    // set amount of refugees
+    var amount = +d.amount
 
-        svgTooltipTimelineTotal.select("text.y1Timeline")
-            .attr("transform", "translate(" + xTooltipTimeline + "," + yTooltipTimeline + ")")
-            .text("Amount of refugees: " + amount.toLocaleString());
+    // place circle on correct place
+    svgTooltipTimelineTotal.select("circle.yTooltip")
+        .attr("transform", "translate(" + xG(d.year) + "," + yG(d.amount) + ")");
 
+    // place correct text in tooltip
+    if (absPercTotal == "absolute values") {
         svgTooltipTimelineTotal.select("text.textAmount")
             .attr("transform", "translate(" + xTooltipTimeline + "," + yTooltipTimeline + ")")
             .text("Amount of refugees: " + amount.toLocaleString());
-
-        svgTooltipTimelineTotal.select("text.y3")
-            .attr("transform", "translate(" + xTooltipTimeline + "," + yTooltipTimeline + ")")
-            .text("Year: " + d.yearx);
-
-        svgTooltipTimelineTotal.select("text.textYear")
-            .attr("transform", "translate(" + xTooltipTimeline + "," + yTooltipTimeline + ")")
-            .text("Year: " + d.yearx);
-
-        svgTooltipTimelineTotal.select(".xTooltip")
-            .attr("transform", "translate(" + xG(d.year) + "," + yG(d.amount) + ")")
-            .attr("y2", heightG - yG(d.amount));
-
-        svgTooltipTimelineTotal.select(".yTooltip")
-            .attr("transform", "translate(" + widthG * -1 + "," + yG(d.amount) + ")")
-            .attr("x2", widthG + widthG);
     }
+    else if (absPercTotal == "percentage of population") {
+        svgTooltipTimelineTotal.select("text.textAmount")
+            .attr("transform", "translate(" + xTooltipTimeline + "," + yTooltipTimeline + ")")
+            .text("Percentage of population: " + amount.toLocaleString());    
+    }
+
+    // place correct year in tooltip
+    svgTooltipTimelineTotal.select("text.textYear")
+        .attr("transform", "translate(" + xTooltipTimeline + "," + yTooltipTimeline + ")")
+        .text("Year: " + d.yearx);
+
+    // place vertical line on correct place
+    svgTooltipTimelineTotal.select(".xTooltip")
+        .attr("transform", "translate(" + xG(d.year) + "," + yG(d.amount) + ")")
+        .attr("y2", heightG - yG(d.amount));
+
+    // place horizontal line on correct place
+    svgTooltipTimelineTotal.select(".yTooltip")
+        .attr("transform", "translate(" + widthG * -1 + "," + yG(d.amount) + ")")
+        .attr("x2", widthG + widthG);
 };
