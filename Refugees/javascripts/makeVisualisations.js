55fb2dfa71f38ab3c085dcdf9b8554874aa64342
smeber <sanne@berendschot.nl>
2017-06-29 18:32:06 +0200

    Update makeVisualisations.js

--- Refugees/javascripts/makeVisualisations.js
+++ Refugees/javascripts/makeVisualisations.js
@@ -4,10 +4,10 @@ Minor Programmeren, Programmeerproject*/
 
 // load data
 queue()
-    .defer(d3.tsv, "Dataset Origin goed 23 juni.tsv")   
-    .defer(d3.tsv, "Dataset Asylum goed 23 juni.tsv")
-    .defer(d3.tsv, "Data Population Worldbank.tsv")    
-    .defer(d3.json, "dataBarchart.json")    
+    .defer(d3.tsv, "https://raw.githubusercontent.com/smeber/Programmeerproject/master/Refugees/data/dataOrigin.tsv")   
+    .defer(d3.tsv, "https://raw.githubusercontent.com/smeber/Programmeerproject/master/Refugees/data/dataAsylum.tsv")
+    .defer(d3.tsv, "https://raw.githubusercontent.com/smeber/Programmeerproject/master/Refugees/data/dataPopulation.tsv")
+    .defer(d3.json, "https://raw.githubusercontent.com/smeber/Programmeerproject/master/Refugees/data/dataBarchart.json")
     .await(makeVisualisations)
 
 // make all data visualisations
